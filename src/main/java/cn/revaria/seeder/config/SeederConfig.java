f9d90e98fc85235878b1f2d31cae6a8e45dfe6fa
CPTProgrammer <46586216+CPTProgrammer@users.noreply.github.com>
2023-12-30 09:58:18 +0800

    Update config

--- src/main/java/cn/revaria/seeder/config/SeederConfig.java
+++ src/main/java/cn/revaria/seeder/config/SeederConfig.java
@@ -3,15 +3,17 @@ package cn.revaria.seeder.config;
 import eu.midnightdust.lib.config.MidnightConfig;
 
 public class SeederConfig extends MidnightConfig {
-    @Entry(category = "text")
+    public static final String LOCATE_CATEGORY = "locate";
+
+    @Comment(category = LOCATE_CATEGORY, centered = true)
     public static Comment locateBiome;
 
-    @Entry(category = "numbers")
+    @Entry(category = LOCATE_CATEGORY)
     public static int locateBiomeDefaultRadius = 6400;
 
-    @Entry(category = "numbers")
+    @Entry(category = LOCATE_CATEGORY)
     public static int locateBiomeDefaultHorizontalBlockCheckInterval = 32;
 
-    @Entry(category = "numbers")
+    @Entry(category = LOCATE_CATEGORY)
     public static int locateBiomeDefaultVerticalBlockCheckInterval = 64;
 }
