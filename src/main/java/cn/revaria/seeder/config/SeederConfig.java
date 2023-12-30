9afd3bedd036f059f95849139e2d83409ec22a88
CPTProgrammer <46586216+CPTProgrammer@users.noreply.github.com>
2023-12-30 12:27:03 +0800

    Update config

--- src/main/java/cn/revaria/seeder/config/SeederConfig.java
+++ src/main/java/cn/revaria/seeder/config/SeederConfig.java
@@ -8,12 +8,15 @@ public class SeederConfig extends MidnightConfig {
     @Comment(category = LOCATE_CATEGORY, centered = true)
     public static Comment locateBiome;
 
-    @Entry(category = LOCATE_CATEGORY)
+    @Entry(category = LOCATE_CATEGORY, min = 1)
     public static int locateBiomeDefaultRadius = 6400;
 
-    @Entry(category = LOCATE_CATEGORY)
+    @Entry(category = LOCATE_CATEGORY, min = 1)
     public static int locateBiomeDefaultHorizontalBlockCheckInterval = 32;
 
-    @Entry(category = LOCATE_CATEGORY)
+    @Entry(category = LOCATE_CATEGORY, min = 1, max = 256)
     public static int locateBiomeDefaultVerticalBlockCheckInterval = 64;
+
+    @Entry(category = LOCATE_CATEGORY, min = 1, max = 128)
+    public static int locateBiomeThreadCount = 4;
 }
